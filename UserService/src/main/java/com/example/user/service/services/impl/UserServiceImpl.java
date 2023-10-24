deab09bff9a95e25cedf187cc0a82180b665e701
aishwarya patle <aishwaryapatle22@gmail.com>
2023-10-24 15:51:15 +0530

    Using Feign Client

--- UserService/src/main/java/com/example/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/example/user/service/services/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.user.service.services.impl;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -16,6 +15,7 @@ import com.example.user.service.entities.Hotel;
 import com.example.user.service.entities.Rating;
 import com.example.user.service.entities.User;
 import com.example.user.service.exceptions.ResourceNotFoundException;
+import com.example.user.service.extenal.services.HotelService;
 import com.example.user.service.repositories.UserRepository;
 import com.example.user.service.services.UserService;
 
@@ -27,6 +27,9 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private HotelService hotelService;
 	
 	private Logger logger = org.slf4j.LoggerFactory.getLogger(UserServiceImpl.class);
 
@@ -52,9 +55,7 @@ public class UserServiceImpl implements UserService {
 		
 		//fetch rating of the above user from RATING SERVICE
 		//http://localhost:8083/ratings/users/c0ad7b06-11c0-45f7-9f20-fe0df2ead6a9
-		
-		//ArrayList forObject = restTemplate.getForObject("http://localhost:8083/ratings/users/c0ad7b06-11c0-45f7-9f20-fe0df2ead6a9", ArrayList.class);
-		//logger.info("{} ", forObject);
+
 		
 		Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/"+user.getUserId(), Rating[].class);
 		logger.info("{} ",ratingsOfUser);
@@ -67,11 +68,8 @@ public class UserServiceImpl implements UserService {
 			//api call to hotel service to get the hotel
 			//http://localhost:8082/hotels/hotelId
 			
-			ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
-			Hotel hotel = forEntity.getBody();
-			
-			logger.info("response status code: {}",forEntity.getStatusCode());
-			
+			Hotel hotel = hotelService.getHotel(rating.getHotelId());
+						
 			//set the hotel to rating
 			rating.setHotel(hotel);
 			
