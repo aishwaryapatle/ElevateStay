785a9107a1a3b7acc6642e6147192eb982cb03ab
aishwarya patle <aishwaryapatle22@gmail.com>
2023-10-24 18:24:21 +0530

    Feign Client POST PUT DELETE Http Calls

--- UserService/src/test/java/com/example/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/example/user/service/UserServiceApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.user.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.user.service.entities.Rating;
+import com.example.user.service.extenal.services.RatingService;
 
 @SpringBootTest
 class UserServiceApplicationTests {
@@ -10,4 +14,14 @@ class UserServiceApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private RatingService ratingService;
+	
+	@Test
+	void createRating() {
+		Rating rating = Rating.builder().rating(10).userId("").hotelId("").feedback("This is created using feign client").build();
+		Rating savedRating = ratingService.createRating(rating);
+		
+		System.out.println("New Rating Created!");
+	}
 }
